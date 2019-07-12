f1384dfc7a3942ce53c52d50329bba723e8016f3
Muhammad Alaraby <mohamedelaraby1995@gmail.com>
2019-07-12 09:42:35 +0200

    Variables and constants

---
+++ YouCv.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "hello";
+echo "dhgdfgdf";
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>YouCv - A personal website which you can create your own cv.</title>
+    </head>
+    <body>
+    
+    </body>
+</html>
